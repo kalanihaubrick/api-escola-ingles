5b722f1833deb74e23f0cfc27c4efa9b9f546cc9
kalanihaubrick <kalanihm@yahoo.com.br>
2023-01-31 20:08:29 -0300

    implementando metodo para cancelar matriculas de uma pessoa

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -197,6 +197,21 @@ class PessoaController {
         }
     }
 
+    static async cancelaPessoa(req, res) {
+        const { estudanteId } = req.params;
+
+        try {
+            await database.Pessoas
+                .update({ ativo: false }, { where: { id: Number(estudanteId) } })
+            await database.Matriculas
+                .update({ status: 'cancelado' }, { where: { estudante_id: Number(estudanteId) } })
+            return res.status(200).json({ message: `Matrículas referente ao estudante ${estudanteId} canceladas` })
+        } catch (error) {
+            res.status(500).json(error.message)
+
+        }
+    }
+
 
 }
 
